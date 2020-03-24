f40bdc8f8e7028d5481b5ba9db25c59a76e232a5
Bobrovsky Dmitry <ministrbob777@gmail.com>
2020-03-24 13:47:20 +0300

    ADD: now we can share Command List files

---
+++ CentOS8.py
@@ -0,0 +1,50 @@
+# Command List file, used for CentOS 8
+
+# Get hostname (for test)
+# command_list = """hostname
+# """
+
+# Rename server
+# command_list = """hostnamectl set-hostname {hostname}
+# hostname
+# """
+
+# Turn off Selinux
+# command_list = """sed -i -- s/=enforcing/=disabled/g /etc/selinux/config
+# reboot
+# """
+
+# DNS settings
+# command_list = """nmcli connection modify eno5 ipv4.dns "10.128.241.101 10.128.241.102"
+# nmcli connection modify eno5 ipv4.dns-search "dc1.local"
+# systemctl restart NetworkManager
+# nmcli connection show eno5 | grep ipv4.dns
+# cat /etc/resolv.conf
+# """
+
+# Time sync
+# command_list = """sudo chmod 777 /etc/chrony.conf
+# sudo sed -i.bak -e "s/pool 2.centos.pool.ntp.org iburst/# pool 2.centos.pool.ntp.org iburst\\nserver 10.128.1.1 iburst/g" /etc/chrony.conf
+# sudo chmod 644 /etc/chrony.conf
+# sudo chmod 644 /etc/chrony.conf.bak
+# head -5 /etc/chrony.conf
+# sudo systemctl enable chronyd
+# sudo systemctl start chronyd
+# chronyc sourcestats
+# """
+
+# Time sync (test)
+# command_list = """chronyc sourcestats
+# """
+
+# Turn off cockpit
+# command_list = """sudo systemctl disable --now cockpit.socket
+# """
+
+# Выключить консоль управления
+# command_list = """dnf install yum-utils
+# yum-config-manager --enable AppStream
+# yum-config-manager --enable BaseOS
+# yum-config-manager --enable extras
+# """
+
