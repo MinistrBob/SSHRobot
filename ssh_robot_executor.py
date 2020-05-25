db54b5c4f890685785482cbcff96a65007ca0e63
MinistrBob <ministrbob777@gmail.com>
2020-05-25 19:45:45 +0300

    Final report

--- ssh_robot_executor.py
+++ ssh_robot_executor.py
@@ -5,7 +5,9 @@ import SETTINGS
 commands = SETTINGS.cl.splitlines()
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-error_count = 0
+list_error_connect = []
+list_error_execute = []
+list_success = []
 for ip, param in SETTINGS.sl.items():
     print((' Host: ' + ip + ' ').center(60, '-'))
     if 'port' in param:
@@ -16,10 +18,12 @@ for ip, param in SETTINGS.sl.items():
     except (ConnectionError, TimeoutError) as e:
         print(f"ERROR: Can't connect to {ip}")
         print(e)
+        list_error_connect.append((ip, e))
         continue
     except Exception as e:
         print(f"ERROR: Can't connect to {ip}")
         print(traceback.format_exc())
+        list_error_connect.append((ip, e))
         continue
 
     for command in commands:
@@ -29,13 +33,13 @@ for ip, param in SETTINGS.sl.items():
         # command = command.replace("%hosthame%", param['hostname'])
         print(f"# {command}")
         try:
-            stdin, stdout, stderr = ssh.exec_command(command)
+            stdin, stdout, stderr = ssh.exec_command(command, get_pty=True)
             stdin.write(param['password'] + '\n')
             stdin.flush()
             err = stderr.read().decode('utf-8')
             out = stdout.read().decode('utf-8')
             if len(out) > 0:
-                print(f"{stdout.read().decode('utf-8')}")
+                print(out)
             else:
                 if SETTINGS.replace_empty_out_str_with_null:
                     print(SETTINGS.replace_empty_out_str_template)
@@ -54,14 +58,30 @@ for ip, param in SETTINGS.sl.items():
                     print(f"ERROR: Execute command: {command}")
                     print(f"{err}")
                     print(f"Execution break")
-                    error_count += 1
+                    list_error_execute.append((ip, err))
                     break
         except Exception as e:
             print(f"ERROR: Execute command: {command}")
             print(traceback.format_exc())
             print(f"Execution break")
+            list_error_execute.append((ip, e))
             break
     ssh.close()
+    list_success.append(ip)
     print('-' * 60)
-if error_count > 0:
-    print(f"!!! THERE WERE {error_count} ERRORS")
+total = len(SETTINGS.sl)
+success = len(list_success)
+error_c = len(list_error_connect)
+error_e = len(list_error_execute)
+if success > 0:
+    print(f"The commands was executed successfully on {success}/{total}={round(success*100/total)}% servers:")
+    for i in list_success:
+        print(f"    {i}")
+if error_c > 0:
+    print(f"Failed to connect to {error_c}/{total}={round(error_c*100/total)}% servers:")
+    for i in list_error_connect:
+        print(f"    {i}")
+if error_e > 0:
+    print(f"Failed to connect to {error_e}/{total}={round(error_e*100/total)}% servers:")
+    for i in list_error_execute:
+        print(f"    {i}")
